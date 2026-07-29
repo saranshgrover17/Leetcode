c26dd4bd20e0690f3a36a90051682d1b03daccc6
Saransh Grover <anshgrv17@gmail.com>
2026-07-30 03:12:37 +0530

    Time: 14 ms (92.04%) | Memory: 51.6 MB (19.28%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,22 +1,17 @@
 class Solution {
     public static int findContentChildren(int[] g, int[] s) {
         int count = 0 ;
-        TreeMap<Integer,Integer> mpp = new TreeMap<>();
-        for(int i = 0 ; i < s.length ; i++){
-            mpp.put(s[i], mpp.getOrDefault(s[i], 0)+1);
-        }
+        Arrays.sort(s);
+        Arrays.sort(g);
+        int i = 0 ;
+        int j = 0;
 
-        for(int i = 0 ; i < g.length ; i++){
-            for(int key : mpp.keySet()){
-                if(key>=g[i]){
-                    count++;
-                    mpp.put(key, mpp.get(key)-1);
-                    if(mpp.get(key)==0){
-                        mpp.remove(key);
-                    }
-                    break;
-                }
+        while(i<g.length && j < s.length){
+            if(s[j]>=g[i]){
+                count++;
+                i++;
             }
+            j++;
         }
         return count;
     }
