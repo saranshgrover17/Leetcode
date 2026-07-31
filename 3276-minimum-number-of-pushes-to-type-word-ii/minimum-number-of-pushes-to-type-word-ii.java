79a55ec671d6a67b4c922ef310a976f415815b69
Saransh Grover <anshgrv17@gmail.com>
2026-08-01 02:42:47 +0530

    Time: 8 ms (98.08%) | Memory: 47.6 MB (96.72%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,44 +1,29 @@
 class Solution {
     public static int minimumPushes(String s) {
         int ans = 0;
-        HashMap<Character, Integer> mpp = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            mpp.put(s.charAt(i), mpp.getOrDefault(s.charAt(i), 0) + 1);
-        }
-        int[] arr = new int[mpp.size()];
-        int j = 0;
-        for (char key : mpp.keySet()) {
-            arr[j] = mpp.get(key);
-            j++;
+        int[] arr = new int[26];
+        for(int i = 0 ; i < s.length() ; i++){
+            arr[s.charAt(i)-'a']++;
         }
         Arrays.sort(arr);
-        int m = 0;
-        int n = arr.length - 1;
-
-        while (m < n) {
-            int temp = arr[m] ;
-            arr[m] = arr[n];
-            arr[n] = temp;
-            m++;
-            n--;     
-        }
-
         int count = 1;
-        for (int k = 0; k < arr.length; k++) {
-            if (count <= 8) {
-                ans = ans + arr[k];
-                count++;
-            } else if (count <= 16) {
-                ans = ans + 2 * arr[k];
-                count++;
-            } else if (count <= 24) {
-                ans = ans + 3 * arr[k];
-                count++;
-            } else {
-                ans = ans + 4 * arr[k];
+        for(int i = arr.length-1 ; i>=0 ; i--){
+            if(count<=8){
+                ans = ans + arr[i];
                 count++;
             }
-
+            else if (count<=16){
+                ans = ans + 2*arr[i];
+                count++;
+            }
+            else if (count<=24){
+                ans = ans + 3*arr[i];
+                count++;
+            }
+            else {
+                ans = ans + 4*arr[i];
+                count++;
+            }
         }
         return ans;
     }
