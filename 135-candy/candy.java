9df337b2e4d65d2033f35adb656a1beff315ca12
Saransh Grover <anshgrv17@gmail.com>
2026-08-03 03:38:54 +0530

    Time: 3 ms (86.44%) | Memory: 47.7 MB (32.32%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static int candy(int[] arr) {
+        int[] ans = new int[arr.length];
+        Arrays.fill(ans, 1);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                ans[i] = Math.max(ans[i], ans[i - 1]+1);
+            }
+        }
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if(arr[i]>arr[i+1]){
+                ans[i] = Math.max(ans[i], ans[i+1]+1);
+            }
+        }   
+        int sum = 0;
+        for(int i = 0 ; i < ans.length ; i++){
+            sum = sum + ans[i];
+        }
+        return sum;
+    }
+}
