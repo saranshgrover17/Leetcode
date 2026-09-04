28620b49c4e0e0d70561b4acaa9463d5d167f516
Saransh Grover <anshgrv17@gmail.com>
2026-09-04 23:24:00 +0530

    Time: 1 ms (99.79%) | Memory: 46.2 MB (88.30%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -2,20 +2,16 @@ class Solution {
     public static int firstStableIndex(int[] arr, int k) {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        int[] maxx = new int[arr.length];
         int[] minn = new int[arr.length];
-
-        for(int i = 0 ; i < arr.length ; i++){
-            maxx[i] = Math.max(max, arr[i]);
-            max = Math.max(max, arr[i]);
-        }
+        
         for(int i = arr.length-1 ; i >=0 ; i--){
             minn[i] = Math.min(min,arr[i]);
             min = Math.min(min,arr[i]);
         }
 
-        for(int i = 0 ; i < maxx.length ; i++){
-            if((maxx[i]-minn[i])<=k){
+        for(int i = 0 ; i < arr.length ; i++){
+            max = Math.max(max, arr[i]);
+            if((max-minn[i])<=k){
                 return i;
             }
         }
