2ccd9c500545dd34165421abdac74a381f28016f
Saransh Grover <anshgrv17@gmail.com>
2026-09-04 15:07:01 +0530

    Time: 2 ms (100.00%) | Memory: 121.9 MB (13.07%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -1,49 +1,17 @@
 class Solution {
     public static boolean uniformArray(int[] arr) {
-        int even = 0;
-        int odd = 0;
+        int minel = Integer.MAX_VALUE;
         for(int i = 0 ; i < arr.length ; i++){
-            if(arr[i]%2==0){
-                even++;
-            }
-            else if(arr[i]%2!=0){
-                odd++;
-            }
+            minel = Math.min(minel, arr[i]);
         }
-
-        if(even==0 || odd == 0){
+        if(minel%2!=0){
             return true;
         }
-
-        int minodd = Integer.MAX_VALUE;
         for(int i = 0 ; i < arr.length ; i++){
-            if(arr[i]%2!=0){
-                minodd = Math.min(minodd, arr[i]);
+            if(arr[i]%2==1){
+                return false;
             }
         }
-
-        if(even>0 && odd>0){
-            odd = 0;
-            even = 0;
-
-            for(int i = 0 ; i < arr.length ; i++){
-                if(arr[i]%2!=0){
-                    odd++;
-                }
-                else if(arr[i]%2==0){
-                    boolean check = false;
-                    if((arr[i]-minodd)>=1 &&(arr[i]-minodd)%2!=0 ){
-                        check = true;
-                    }
-                    if(check == false){
-                        return false;
-                    }
-                }
-            }
-        }
-        if(even==0 || odd == 0){
-            return true;
-        }
-        return false;
+        return true;
     }
 }
