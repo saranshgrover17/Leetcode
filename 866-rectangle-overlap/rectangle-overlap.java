86d7969c1f5d1fcaec0cfd9ef5bf62b9528d2bb7
Saransh Grover <anshgrv17@gmail.com>
2026-09-20 04:28:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (16.43%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.java
+++ 866-rectangle-overlap/rectangle-overlap.java
@@ -1,27 +1,8 @@
 class Solution {
     public static boolean isRectangleOverlap(int[] arr1, int[] arr2) {
-        int r1x1 = arr1[0];
-        int r1y1 = arr1[1];
-        int r1x2 = arr1[2];
-        int r1y2 = arr1[3];
+        int w = Math.min(arr1[2], arr2[2]) - Math.max(arr1[0], arr2[0]);
+        int h = Math.min(arr1[3], arr2[3]) - Math.max(arr1[1], arr2[1]);
 
-        int r2y1 = arr2[1];
-        int r2x1 = arr2[0];
-        int r2x2 = arr2[2];
-        int r2y2 = arr2[3];
-
-        if (r2x1 >= r1x2) {
-            return false;
-        }
-        if (r2y1 >= r1y2) {
-            return false;
-        }
-        if (r1x1 >= r2x2) {
-            return false;
-        }
-        if (r1y1 >= r2y2) {
-            return false;
-        }
-        return true;
+        return w>0 && h>0;
     }
 }
