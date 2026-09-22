f1cc5c3b8652f38e119a97ee1620252f07c95038
Saransh Grover <anshgrv17@gmail.com>
2026-09-22 05:40:38 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (75.46%) - LeetSync

--- 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.java
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.java
@@ -1,20 +1,26 @@
 class Solution {
     public static boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
-        if (radius == 1415 && xCenter == 807 && yCenter == -784 && x1 == -733 && y1 == 623 && x2 == -533 && y2 == 1005) {
-            return false;
+        int xi,yi;
+        if(x1>xCenter){
+            xi = x1;
         }
-        if (x1 > xCenter + radius) {
-            return false;
+        else if(x2<xCenter){
+            xi = x2;
         }
-        if (x2 < xCenter - radius) {
-            return false;
+        else {
+            xi = xCenter;
         }
-        if (y1 > yCenter + radius) {
-            return false;
+
+        if(y1>yCenter){
+            yi = y1;
         }
-        if (y2 < yCenter - radius) {
-            return false;
+        else if(y2<yCenter){
+            yi = y2;
         }
-        return true;
+        else {
+            yi = yCenter;
+        }
+        int distance = (xi-xCenter)*(xi-xCenter)+(yi-yCenter)*(yi-yCenter);
+        return distance<=radius*radius;
     }
 }
